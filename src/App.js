3421add464eb2fd053d0b5209d947c7e3801d84e
Mariano <mariano_ariel_97@outlook.com>
2022-05-19 17:34:57 -0300

    Desafio menu e-commerce

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import { Navbar } from "./components/Navbar"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Coderhouse</h1>
-        <p>
-          Curso React js. 
-        </p>
-        <a
-          className="App-link"
-          href="https://www.coderhouse.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Ir a Coderhouse
-        </a>
-      </header>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import { AiFillHome, AiFillInfoCircle, AiFillCloseCircle } from 'react-icons/ai'; 
+import { BsFillCartCheckFill } from 'react-icons/bs';
+import { IoIosListBox } from 'react-icons/io';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import React, { useState } from 'react';
+
+export const Navbar = () => {
+    const [estado, setEstado] = useState(false)
+
+    const cambiarEstado = () =>{
+      console.log(estado);
+      estado ? setEstado(false) : setEstado(true);
+      
+      
+    }
+    return <header className={`header ${estado ? "active-header":""}`.trimEnd() }>
+      <h1 className="nombre">compupro</h1>
+      <nav className="menu">
+        <ul className="lista">
+          {/*evitar vulnerabilidades con "rel="noreferrer"*/}
+          <a className="botones-menu active" href="https://www.youtube.com/" target="_blank" rel="noreferrer"><AiFillHome className="iconos" /><li>Inicio</li></a>
+          <a className="botones-menu" href="https://www.youtube.com/" target="_blank" rel="noreferrer"><IoIosListBox className="iconos" /><li>Productos</li></a>
+          <a className="botones-menu" href="https://www.youtube.com/" target="_blank" rel="noreferrer"><BsFillCartCheckFill className="iconos" /><li>Carrito</li></a>
+          <a className="botones-menu" href="https://www.youtube.com/" target="_blank" rel="noreferrer"><AiFillInfoCircle className="iconos" /><li>Información</li></a>
+        </ul>
+      </nav>
+      <div className="header-mobile" onClick={cambiarEstado} >
+        <AiFillCloseCircle className="header-mobile-icon iconos acciones" id="cerrar"  />
+        <GiHamburgerMenu className="header-mobile-icon iconos acciones" id="abrir" />
+      </div>
+    </header>
+}
